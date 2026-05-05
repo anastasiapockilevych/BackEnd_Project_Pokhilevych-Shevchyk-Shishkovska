a222a4d3b9b5d5856b2d875a14ccd245d0e50f37
shyshkovskaliliia <shyshkovska.liliia@chnu.edu.ua>
2026-05-05 21:07:12 +0300

    update app

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const pollsRouter = require('./routes/polls');
+const votesRouter = require('./routes/votes');
 require('./config/db.config'); 
 
 const app = express();
@@ -32,9 +34,7 @@ const swaggerOptions = {
 
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-// ---------------------------------------------
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -46,20 +46,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/polls', pollsRouter);
+app.use('/votes', votesRouter);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
     next(createError(404));
 });
 
-// error handler
-// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
